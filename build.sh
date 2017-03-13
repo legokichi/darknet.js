1b994d2b1790bc090ecedab7668a9e48fdeec507
legokichi <legokichi@gmail.com>
2017-03-13 17:16:23 +0900

    update

--- build.sh
+++ build.sh
@@ -5,3 +5,7 @@ mkdir weights
 wget http://pjreddie.com/media/files/tiny.weights -P weights
 emmake make
 sed -in-place 's/PthreadWorkerInit:PthreadWorkerInit});PThread.unusedWorkerPool.push/PthreadWorkerInit:PthreadWorkerInit},[HEAPU8.buffer]);PThread.unusedWorkerPool.push/' darknet.js
+
+# https://pjreddie.com/darknet/tiny-darknet/
+# ./darknet classify cfg/tiny.cfg weights/tiny.weights data/dog.jpg
+# Module.callMain(["classify", "cfg/tiny.cfg", "weights/tiny.weights", "data/dog.jpg"])
